ec17f1b124daf825603120451512542d5af436d1
Rob Winch <rwinch@users.noreply.github.com>
2024-01-23 22:14:12 -0600

    public class SpringBootApplicationMain

--- spring-boot-testjars/src/main/java/org/springframework/experimental/boot/server/exec/main/SpringBootApplicationMain.java
+++ spring-boot-testjars/src/main/java/org/springframework/experimental/boot/server/exec/main/SpringBootApplicationMain.java
@@ -20,7 +20,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class SpringBootApplicationMain {
+public class SpringBootApplicationMain {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApplicationMain.class, args);
